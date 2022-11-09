0cf371a5a44cab0b7ff3130ed6c9908657a6ec2f
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-11-08 18:08:51 -0700

    Add bootstrap to project

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,12 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+      <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+          integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+          crossOrigin="anonymous"
+      />
   </React.StrictMode>
 );
 
