43b6950f5fe8f6230a80a263014c55c1803dc212
eggmangrg <aagaman@ualberta.ca>
2022-11-14 15:14:05 -0700

    More sketch changes

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,40 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 
 function Login() {
-    return (
-      <div style={{ paddingBottom: 100, paddingTop: 100 }}><div style={{ textAlign: "center" ,}}>
-        <img src="https://image.shutterstock.com/image-vector/plate-vector-illustrationisolated-on-white-260nw-1815162875.jpg" ></img>
-        <h1>
-          DishZero
-        </h1>
-        <h2> Insert Catch Phrase
-          </h2></div>
-        <div style={{textAlign:"left", paddingLeft: 250}}>
-          <form>
-          <h2>Email</h2>
-          <input type="text" value ="@ualberta.ca">
-          </input>
-          <h2>
-            Password
-          </h2>
-          <input type="text">
-          </input></form>
-          <br></br></div>
-          <div style={{ textAlign: "center" }}>
-            <br></br>
-          <button>
-            Login
-          </button>
-          <h2>---- OR ----</h2>
-          
-          <button>
-            Login with Google
-          </button>
-          </div>
-        </div>
-    );
-  }
+  return (
+    <div style={{ paddingBottom: 100, paddingTop: 100 }}>
+      <div style = {{textAlign:"center"}}>
+      <img src="https://image.shutterstock.com/image-vector/plate-vector-illustrationisolated-on-white-260nw-1815162875.jpg" ></img>
+      <h1>DishZero</h1>
+      <h2>Catch Phrase</h2>
+      <Link to ="/Login"><button>Login</button></Link>
+      <h3>Don't have account? <a href="/SignUp">Sign up</a></h3>
+      </div></div>
+  );
+}
   
   export default Login;
   
