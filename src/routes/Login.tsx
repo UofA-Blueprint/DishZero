6902afefe7810f37e21e441cd59268ee08682a83
eggmangrg <aagaman@ualberta.ca>
2022-11-15 20:37:01 -0700

    Match sketch login page

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,19 +1,46 @@
-import React from "react";
-import { Link } from 'react-router-dom'
-
+import React from 'react';
+import logo from './logo.svg';
 
 function Login() {
-  return (
-    <div style={{ paddingBottom: 100, paddingTop: 100 }}>
-      <div style = {{textAlign:"center"}}>
-      <img src="https://image.shutterstock.com/image-vector/plate-vector-illustrationisolated-on-white-260nw-1815162875.jpg" ></img>
-      <h1>DishZero</h1>
-      <h2>Catch Phrase</h2>
-      <Link to ="/Login"><button>Login</button></Link>
-      <h3>Don't have account? <a href="/SignUp">Sign up</a></h3>
-      </div></div>
-  );
+
+    return (
+        <div
+            style={{
+            paddingBottom: 100,
+            paddingTop: 10
+        }}>
+            <div style={{
+                textAlign: "center"
+            }}>
+                <img
+                    src="https://image.shutterstock.com/image-vector/plate-vector-illustrationisolated-on-white-260nw-1815162875.jpg"></img>
+                <h1>
+                    DishZero
+                </h1>
+                <h2>
+                    Insert Catch Phrase
+                </h2>
+                <form>
+                    <h2>Email</h2>
+                    <input type="text" defaultValue="@ualberta.ca"></input>
+                    <h2>
+                        Password
+                    </h2>
+                    <input type="password"></input>
+                </form>
+                <br></br>
+            </div>
+            <div style={{
+                textAlign: "center"
+            }}>
+                <br></br>
+                <button>Login</button>
+                <h2>---- OR ----</h2>
+                <button>
+                    Login with Google
+                </button>
+            </div>
+        </div>
+    );
 }
-  
-  export default Login;
-  
+export default Login;
