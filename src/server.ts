1b5811af164208454a4681a70d05c7a8be8a244c
Afraz Khan <afrazk21@gmail.com>
2022-11-16 19:27:51 -0700

    added express boilerplate and created server.ts

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// config and init
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running successfully on port ${PORT}`);
+});
+
+
+
+export {};
