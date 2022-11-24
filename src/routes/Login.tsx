4877e5d4e4cca67683062c2d27aab6685c10fd97
eggmangrg <aagaman@ualberta.ca>
2022-11-23 18:47:54 -0700

    Sidebar menu

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -8,33 +8,41 @@ import {
     Circle,
     Line
 } from 'react-konva';
+import { Sidebar } from '../widgets/sidebar';
 function Login() {
 
     return (
         <div
+            className='App'
+            id='outer-container'
             style={{
             paddingBottom: 50,
             paddingTop: 10,
             textAlign: "center"
         }}>
-            <img width={100} height={100}
-                src="https://image.shutterstock.com/image-vector/plate-vector-illustrationisolated-on-white-260nw-1815162875.jpg"></img>
-            <h1 style={{
-                fontWeight: "bolder"
-            }}>
-                DishZero
-            </h1>
-            <h3 >
-                Helping the planet one dish<br></br>
-                at a time
-            </h3>
-            <Stage width={window.innerWidth} height={200}>
-                <Layer>
-                    <Rect x={125} y={0} width={150} height={150} fill="#D6D6D6"/>
-                </Layer>
-            </Stage>
-            <br></br>
-            <button>Login with Google</button>
+         <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
+            <div id='page-wrap'>
+                <img
+                    width={100}
+                    height={100}
+                    src="https://image.shutterstock.com/image-vector/plate-vector-illustrationisolated-on-white-260nw-1815162875.jpg"></img>
+                <h1 style={{
+                    fontWeight: "bolder"
+                }}>
+                    DishZero
+                </h1>
+                <h3 >
+                    Helping the planet one dish<br></br>
+                    at a time
+                </h3>
+                <Stage width={window.innerWidth} height={200}>
+                    <Layer>
+                        <Rect x={125} y={0} width={150} height={150} fill="#D6D6D6"/>
+                    </Layer>
+                </Stage>
+                <br></br>
+                <button>Login with Google</button>
+            </div>
         </div>
     );
 }
---
+++ src/widgets/sidebar.tsx
@@ -0,0 +1,69 @@
+import {slide as Menu} from "react-burger-menu";
+import "../styles/sidebar.css"
+
+type SidemenuProps = {
+    pageWrapId: string;
+    outerContainerId: string;
+};
+
+export const Sidebar = ({pageWrapId, outerContainerId} : SidemenuProps) => {
+    return (
+        <Menu>
+            <p
+                className="memu-title"
+                style={{
+                textAlign: "left"
+            }}>
+                <img
+                style={{paddingRight:20}}
+                    width={20}
+                    height={20}
+                    src="https://image.shutterstock.com/image-vector/plate-vector-illustrationisolated-on-white-260nw-1815162875.jpg"></img>DishZero</p>
+            <a className="menu-item" href="/home">
+                <p>MENU</p><img
+                style={{paddingRight:20}}
+                    width={20}
+                    height={20}
+                    src="https://cdn-icons-png.flaticon.com/512/25/25694.png"></img>
+                Home
+            </a>
+            <a className="menu-item" href="/dishes">
+                <img
+                  style={{paddingRight:20}}
+                    width={20}
+                    height={20}
+                    src="https://cdn-icons-png.flaticon.com/512/76/76211.png"></img>
+                My dishes
+            </a>
+            <a
+                className="menu-item"
+                href="/dishes"
+                style={{
+                paddingTop: 280
+            }}>
+                <img
+                    style={{paddingRight:20}}
+                    width={20}
+                    height={20}
+                    src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-clipboard-512.png"></img>
+                How it works
+            </a>
+            <a className="menu-item" href="/dishes">
+                <img
+                style={{paddingRight:20}}
+                    width={20}
+                    height={20}
+                    src="https://static.thenounproject.com/png/4653746-200.png"></img>
+                Our impact
+            </a>
+            <a className="menu-item" href="/dishes">
+                <img
+                style={{paddingRight:20}}
+                    width={20}
+                    height={20}
+                    src="https://www.iconpacks.net/icons/2/free-exit-logout-icon-2857-thumb.png"></img>
+                Logout
+            </a>
+        </Menu>
+    );
+};
