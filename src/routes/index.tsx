e6a7b72223cae9dfd5f1d634afd6386d0cbd7b26
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-11-25 19:13:05 -0700

    Restore orig function of default route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
 import {DishCheckoutLoginCallback, DishCheckout} from "../widgets";
+import Checkout from "./CheckOut";
+import CheckIn from "./CheckIn";
 import Login from './Login';
 
 // Export all routes here
 const router = [
     {
         path: "/",
-        element: <Login />,
+        element: <>
+            <Login />
+            <Checkout />
+            <CheckIn />
+        </>,
     },
     {
         path: "/checkout/:id",
