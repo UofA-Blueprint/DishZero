78369f3f9a884563742e7924b47a5d8f26159d2b
eggmangrg <aagaman@ualberta.ca>
2022-11-27 18:15:12 -0700

    Fix merge conflict with index

--- src/index.tsx
+++ src/index.tsx
@@ -12,42 +12,33 @@ import {
 import Login from './routes/Login';
 import CheckOut from './routes/CheckOut';
 import CheckIn from './routes/CheckIn';
+import CheckOutApi from './widgets/CheckOutApi';
 
 // Considering redux a service which provides a centralized state which can be accessed
 // through any component, we need to add it as a parent for all components. (Hence: <Provider/>)
 
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Login />,
-//   },
-// ]);
-
-// root.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <RouterProvider router={router} />
-//       <Login/>
-//       <CheckOut />
-//       <CheckIn />
-//     </Provider>
-//   </React.StrictMode>
-// );
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Login />
+      <RouterProvider router={router} />
+      {/* <Login /> */}
+      <CheckOut />
+      <CheckIn />
+      {/* <CheckOutApi /> */} {/* Template Component */} 
     </Provider>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
