01eb429b61b87a95584b7b9e9801596e76229ed9
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-11-29 20:19:03 -0700

    Add call handler to the checkout

--- src/widgets/checkout/DishCheckout.tsx
+++ src/widgets/checkout/DishCheckout.tsx
@@ -2,23 +2,33 @@ import {useEffect, useRef, useState} from "react";
 import {useParams} from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 
+import DishApi from "../../stores/api"
+import {db} from "../../firebase"
+
 /**
  * Component that checks out the dish and redirects to the page with the url
  */
-const DishCheckout = ({db}: any) => {
+const DishCheckout = () => {
     let navigate = useNavigate();
     const { id } = useParams();
     // used to ensure that the checkout only happens once
     const idLastCheckedOut = useRef("0");
 
+    const checkoutDish = async function(qid: string) {
+        // TODO: Add handler that does not require the User Id
+        let user = "1";
+        const dish = await DishApi.getDishID(db, user, qid)
+        const docRef = await DishApi.CheckOutDish(db, user, dish.docId);
+        // If user logged in then go to home page, TODO
+        // else 
+        navigate(`/checkout/login/callback/${id}`);
+    }
+
     useEffect(() => {
         if (idLastCheckedOut.current != id) {
             // TODO: Use handler function to update the status of the dish
-            console.log(`Use handler function, checkout: ${id}`)
+            checkoutDish(id || "0")
             idLastCheckedOut.current = id || "0";
-            // If user logged in then go to home page
-            // else
-            navigate(`/checkout/login/callback/${id}`);
         }
     }, [])
     return (
