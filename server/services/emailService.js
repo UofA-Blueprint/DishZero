cd07391b9cdad5ed20eaf6f71785f4d6890c7b57
Meharpreet Singh Nanda <msnanda515@gmail.com>
2023-01-24 22:08:47 -0700

    Update email code

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,31 +1,15 @@
-const nodemailer = require("nodemailer");
-
-const {emailConfig} = require("../config.js");
-
-const transporter = nodemailer.createTransport({
-    service: emailConfig.service,
-    auth: {
-      user: emailConfig.user,
-      pass: emailConfig.pass,
-    }
-});
+const { db } = require("../db.js");
 
 const emailService = {
     sendReminderEmail: async function (to, subject, body) {
         // TODO: SEND EMAIL
         let mailOptions = {
-            from: 'youremail@gmail.com',
             to: to,
             subject: subject,
             text: body,
         };
-        // transporter.sendMail(mailOptions, function(error, info){
-        //     if (error) {
-        //       console.log(error);
-        //     } else {
-        //       console.log('Email sent: ' + info.response);
-        //     }
-        // });
+        // uses firebase mail service to send email
+        // db.collection('mail').add(mailOptions)
         console.log("EMAIL SENT: ", to, subject, body); // TODO: Remove this
     }
 };
