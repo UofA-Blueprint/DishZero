d811b89df850261f02319f076dd42380ae8a0210
Meharpreet Singh Nanda <msnanda515@gmail.com>
2023-02-28 18:15:55 -0700

    Add logo

--- app/src/admin/toolbar.tsx
+++ app/src/admin/toolbar.tsx
@@ -7,7 +7,14 @@ export default () => {
         <>
         <nav id="sidebarMenu" className={`col-md-3 col-lg-2 d-md-block bg-light sidebar collapse`} >
                 <div className={`position-sticky pt-3 sidebar-sticky`}>
+                    
                     <ul className="nav flex-column">
+                        <div className="sidebar-heading d-flex align-items-center px-3 mt-4 mb-1  justify-content-start">
+                            <img src="/admin_logo.svg"/>
+                            <h4>
+                                DishZero
+                            </h4>
+                        </div>
                         <p className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-uppercase">
                             Admin Panel
                         </p>
