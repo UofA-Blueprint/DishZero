7d0539083c49d200e251976c680a7a562d7b618e
Meharpreet Singh Nanda <msnanda515@gmail.com>
2023-03-14 12:21:52 -0600

    Add table row to table

--- app/src/admin/dishList.tsx
+++ app/src/admin/dishList.tsx
@@ -41,6 +41,24 @@ const DishCard = ({dishId, dishType, dishStatus, dishOverdue, dishTimesBorrowed,
     )
 }
 
+const AdminDishTableRow = ({dish, selectedHandler}) => {
+    return (
+        <>
+            <tr>
+                <th scope="row">
+                    <input type="checkbox" />
+                </th>
+                <td>{dish.id}</td>
+                <td>{dish.type}</td>
+                <td>{dish.status}</td>
+                <td>{dish.overdue}</td>
+                <td>{dish.timesBorrowed}</td>
+                <td>{dish.dateAdded}</td>
+            </tr>
+        </>
+    );
+}
+
 const DishData = () =>{
     // get dishlist using useEffect
     const [dishList, SetDishList] = useState(dishes);
@@ -175,25 +193,77 @@ const DishData = () =>{
             </div>
             <div className='dishDashboard-data '>
                 {/* Top text fields */}
-                <div className='d-flex flex-row mb-3 justify-content-between'>
+                <div className='d-flex flex-row mb-2 mt-4 justify-content-between'>
                     <div className='d-flex flex-row mb-3'>
                         <div className="input-group me-3 rounded-pill border border-dark py-1 px-2">
                             <input type="text" className="form-control border-0" placeholder="Type text here" />
                         </div>
-                        <button type="button" className="btn rounded-pill bg-transparen border border-success text-success px-4">
+                        <button type="button" className="btn rounded-pill bg-transparen border border-success text-success px-5">
                             Search
                         </button>
                     </div>
 
-                    <div className='d-flex flex-row mb-3'>
-                        <div className="input-group me-3 rounded-pill border border-dark py-1 px-2">
-                            <input type="text" className="form-control border-0" placeholder="Enter an id" />
-                        </div>
-                        <button type="button" className="btn rounded-pill btn-success px-4">
+                    <div className='d-flex flex-row mb-3 '>
+                        <button type="button" className="btn rounded-pill bg-transparen border border-success text-success px-5 me-3">
+                            Export Data
+                        </button>
+                        <button type="button" className="btn rounded-pill btn-success px-5">
                             + Add new dish
                         </button>
                     </div>
                 </div>
+
+                <table className="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">
+                            <input className="form-check-input mt-0" type="checkbox" />
+                        </th>
+                        <th scope="col">Dish ID</th>
+                        <th scope="col">
+                            <div className="dropdown">
+                                <button className="btn  dropdown-toggle border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dish type  
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                </ul>
+                            </div>
+                        </th>
+                        <th scope="col">
+                            <div className="dropdown">
+                                <button className="btn dropdown-toggle border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Status
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                </ul>
+                            </div>
+                        </th>
+                        <th scope="col">
+                            <button className="btn border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Overdue                                
+                            </button>
+                        </th>
+                        <th scope="col">
+                            <button className="btn border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Times Borrowed                                
+                            </button>
+                        </th>
+                        <th scope="col">
+                            <button className="btn border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Date Added                             
+                            </button>
+                        </th>
+                        
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {shownDishList.map((dish, key) => (
+                            <AdminDishTableRow dish={dish} key={key} selectedHandler={null} />
+                        ))}
+                    </tbody>
+                </table>
 
                 <div className='dishDashboard-header-row'>
                     <input type="checkbox" checked={headerChecked} onChange={handleHeaderCheckChange}/>
