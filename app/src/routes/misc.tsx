44bc41854179b9e4e57706c65e0e3caaa3e6744b
Farel Lukas <66014997+farellukas@users.noreply.github.com>
2023-03-15 20:16:44 -0600

    Styled Error404 page (#199)

--- app/src/routes/misc.tsx
+++ app/src/routes/misc.tsx
@@ -2,40 +2,47 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { FirebaseContext } from "../firebase";
 import leaf_icon from "../assets/leaf.svg";
+import "../styles/error404.css";
 
 const Error404 = () => {
   const fbContext = useContext(FirebaseContext);
 
   return (
-    <>
+    <div className="error404-wrapper d-flex flex-column align-items-center justify-content-center px-4">
       <img src={leaf_icon} alt="" />
-      <h1>404</h1>
-      <h2>Page not found!</h2>
+      <h1 className="my-3 text-center fw-semibold heading-404">404</h1>
+      <h2 className="my-2 text-center fw-medium subheading-404">
+        Page not found!
+      </h2>
 
       {fbContext?.user ? (
         // logged in
         <>
-          <p>
+          <p className="m-0 text-center text-404">
             We're sorry. The page you requested could not be found. Please go
             back to the homepage.
           </p>
-          <Link to="/">
-            <button>Home</button>
+          <Link to="/" className="mt-4">
+            <button type="button" className="btn-404">
+              Home
+            </button>
           </Link>
         </>
       ) : (
         // not logged in
         <>
-          <p>
+          <p className="m-0 text-center text-404">
             We're sorry. The page you requested could not be found. Please
             login.
           </p>
-          <Link to="/">
-            <button>Login</button>
+          <Link to="/" className="mt-4">
+            <button type="button" className="btn-404">
+              Login
+            </button>
           </Link>
         </>
       )}
-    </>
+    </div>
   );
 };
 
