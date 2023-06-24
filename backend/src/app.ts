e94a531c1b3032e63a64f088158632091021882e
armaankatyal <armaankatyal3@gmail.com>
2023-06-24 16:11:31 -0600

    feat: added logging capability

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,27 @@
 import express, { Response, Request, NextFunction } from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import nodeConfig from 'config';
+import pinoHttp from 'pino-http';
 
 const app = express();
 dotenv.config();
 
 app.use(cors());
+app.use(express.json());
+if (process.env.NODE_ENV !== 'test') {
+    app.use(pinoHttp(
+        {
+            transport: {
+                target: 'pino-pretty',
+                options: {
+                    levelFirst: true,
+                    colorize: true,
+                    translateTime: true
+                }
+            }
+        }
+    ));
+}
 
 app.get('/health', (req: Request, res: Response) => {
     res.status(200).send('OK');
