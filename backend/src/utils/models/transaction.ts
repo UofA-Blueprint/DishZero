473fd375ea42da928f1e908c91abb4435fddfe05
Gurveer Sohal <74379063+GurveerSohal@users.noreply.github.com>
2023-07-04 01:20:31 +0530

    feat : /api/dish endpoint (#215)
    * types
    
    * utility functions for dish api
    
    * dish api
    
    * reorganize types
    
    * change case
    
    * add error handling and logging
    
    * move service folder
    
    * fix error and logging change
    
    * change service location again
    
    * message with error

---
+++ backend/src/utils/models/transaction.ts
@@ -0,0 +1,8 @@
+// need more info on returned type
+export type Transaction = {
+    id : string,
+    dishID : string,
+    userID: string,
+    returned : any,
+    timestamp : Date
+}
---
+++ backend/src/utils/models/user.ts
@@ -0,0 +1,5 @@
+export type User = {
+    id : string,
+    role : string,
+    email : string
+}
