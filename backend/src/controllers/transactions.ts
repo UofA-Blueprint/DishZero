43417b656861278e29eb013a8028bafe709b2b08
Aaron <aaronboyd385@gmail.com>
2023-07-06 18:25:35 -0600

    implemented get all transactions

--- backend/src/controllers/transactions.ts
+++ backend/src/controllers/transactions.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
 import { CustomRequest } from '../middlewares/auth'
+import { db } from '../services/firebase'
+import { Transaction } from '../models/transaction'
 
 export const getTransactions = async (req: Request, res: Response) => {
     // only allow admin to get all transactions
@@ -13,5 +15,34 @@ export const getTransactions = async (req: Request, res: Response) => {
         return res.status(403).json({ error: 'forbidden' })
     }
 
-    // TODO: implement get transactions by role logic
+    
+    // get transactions
+    let transactions = <Array<Transaction>>[]
+    try {
+        let transactionsQuerySnapshot = await db.collection('transactions').get()
+        transactionsQuerySnapshot.docs.forEach((doc) => {
+            let data = doc.data()
+            transactions.push({
+                id: doc.id,
+                dishID: data.dish ? data.dish.id : null,
+                userID: data.user,
+                returned: data.returned ? data.returned : {},
+                timestamp: data.timestamp ? data.timestamp.toDate() : null,
+            })
+        })
+    } catch (e) {
+        req.log.error({
+            error: e,
+            message: 'Error when fetching transactions from firebase',
+        })
+        res.status(500).json({ error: 'internal_server_error' })
+        return
+    }
+
+
+
+    req.log.info({ message: 'Sending transactions' })
+    // send response
+    res.status(200).json({ transactions: transactions })
+    return
 }
