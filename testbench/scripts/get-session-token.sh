701d61081f9368437f88ffa5b450743a5df1ec3f
Gurveer Sohal <gurveersohal433@gmail.com>
2023-07-08 21:08:43 +0530

    testing endpoints

---
+++ testbench/scripts/get-session-token.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+source .env
+source .id_token
+source .session_token
+
+if [ "#$ID_TOKEN" == "#" ] || [ "#$SESSION_TOKEN" == "#" ] ; then
+    echo "Run the script to get the tokens first"
+    exit 1;
+fi
+
+curl -i -X POST \
+    -H "x-api-key: $API_KEY" \
+    -H "session-token: $SESSION_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d "{\"idToken\" : \"$ID_TOKEN\"}" \
+    http://localhost:8080/api/auth/login/
+    
