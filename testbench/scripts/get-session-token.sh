b825247e93897ea03611d04d51cf27266e31249e
Gurveer Sohal <gurveersohal433@gmail.com>
2023-07-09 07:01:03 +0530

    no session token needed

--- testbench/scripts/get-session-token.sh
+++ testbench/scripts/get-session-token.sh
@@ -2,16 +2,14 @@
 
 source .env
 source .id_token
-source .session_token
 
-if [ "#$ID_TOKEN" == "#" ] || [ "#$SESSION_TOKEN" == "#" ] ; then
+if [ "#$ID_TOKEN" == "#" ] ; then
     echo "Run the script to get the tokens first"
     exit 1;
 fi
 
 curl -i -X POST \
     -H "x-api-key: $API_KEY" \
-    -H "session-token: $SESSION_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"idToken\" : \"$ID_TOKEN\"}" \
     http://localhost:8080/api/auth/login/
