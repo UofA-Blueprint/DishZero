9b59de0619dc9f6aa0c51e39c985c53848c486ac
B Phan <bphan@Bs-MacBook-Pro.local>
2023-08-04 00:41:22 +0700

    implemented data table

--- app/src/admin/users.tsx
+++ app/src/admin/users.tsx
@@ -2,10 +2,12 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { 
+    Switch,
+    FormControlLabel,
+    TableSortLabel,
     Button,
     Avatar,
     Typography,
-    AppBar,
     Box,
     Paper,
     InputBase,
@@ -26,39 +28,47 @@ import {
     Email as EmailIcon,
     Search as SearchIcon
 } from '@mui/icons-material';
+import { visuallyHidden } from '@mui/utils';
+import axios from "axios";
 //////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////// Declarations ////////////////////////////
-interface Column {
+interface HeadCell {
     id: 'emailAddress' | 'inUse' | 'overdue' | 'role';
     label: string;
     minWidth?: number;
-    align?: 'right';
-    format?: (value: number) => string;
+    align?: 'right' | 'left';
+    numeric: boolean;
 }
 
-const columns: readonly Column[] = [
+const headCells: readonly HeadCell[] = [
     { 
         id: 'emailAddress', 
         label: 'Email Address', 
-        minWidth: 200 
+        minWidth: 200,
+        numeric: false,
+        align: 'left'
     },
     { 
         id: 'inUse', 
         label: 'In Use', 
-        minWidth: 170 
+        minWidth: 170,
+        numeric: true,
+        align: 'right'
     },
     {
-      id: 'overdue',
-      label: 'Overdue',
-      minWidth: 90,
-      align: 'right'
+        id: 'overdue',
+        label: 'Overdue',
+        minWidth: 90,
+        numeric: true,
+        align: 'right'
     },
     {
-      id: 'role',
-      label: 'Role',
-      minWidth: 170,
-      align: 'right'
+        id: 'role',
+        label: 'Role',
+        minWidth: 170,
+        numeric: false,
+        align: 'right'
     }
 ];
 
@@ -82,8 +92,72 @@ const rows = [
     createData('bhphan@ualberta.ca', 2, 1, 'Admin'),
     createData('hoangtru@ualberta.ca', 4, 5, 'Volunteer'),
     createData('dtho@ualberta.ca', 6, 7, 'Basic'),
-    createData('thiminhh@ualberta.ca', 3, 4, 'Basic')
+    createData('thiminhh@ualberta.ca', 3, 4, 'Basic'),
+    createData('one@ualberta.ca', 3, 4, 'Basic'),
+    createData('two@ualberta.ca', 5, 4, 'Basic'),
+    createData('three@ualberta.ca', 3, 4, 'Basic'),
+    createData('four@ualberta.ca', 3, 4, 'Basic'),
+    createData('five@ualberta.ca', 3, 4, 'Basic'),
+    createData('six@ualberta.ca', 3, 4, 'Basic'),
+    createData('seven@ualberta.ca', 3, 4, 'Basic'),
+    createData('eight@ualberta.ca', 3, 4, 'Basic'),
+    createData('nine@ualberta.ca', 3, 4, 'Basic'),
+    createData('ten@ualberta.ca', 3, 4, 'Basic'),
+    createData('eleven@ualberta.ca', 3, 4, 'Basic'),
+    createData('twelve@ualberta.ca', 3, 4, 'Basic'),
+    createData('thirteen@ualberta.ca', 3, 4, 'Basic'),
+    createData('fourteen@ualberta.ca', 3, 4, 'Basic'),
+    createData('fifteen@ualberta.ca', 3, 4, 'Basic'),
+    createData('sixteen@ualberta.ca', 3, 4, 'Basic'),
+    createData('seventeen@ualberta.ca', 3, 4, 'Basic'),
+    createData('eighteen@ualberta.ca', 3, 4, 'Basic'),
+    createData('nineteen@ualberta.ca', 3, 4, 'Basic'),
+    createData('twenty@ualberta.ca', 3, 4, 'Basic'),
+    createData('twentyone@ualberta.ca', 3, 4, 'Basic'),
+    createData('twentytwo@ualberta.ca', 3, 4, 'Basic')
 ];
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string },
+) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
+    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+ 
+interface EnhancedTableProps {
+    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+    order: Order;
+    orderBy: string;
+}
 //////////////////////////////////////////////////////////////
 
 /////////////////////////////// Sub-components ///////////////////////////////
@@ -127,18 +201,88 @@ const Sidebar = () => {
     )
 }
 
+function EnhancedTableHead(props: EnhancedTableProps) {
+    const { order, orderBy, onRequestSort } =
+      props;
+    const createSortHandler =
+      (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+        onRequestSort(event, property);
+      };
+  
+    return (
+      <TableHead >
+        <TableRow>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              align={headCell.align}
+              padding={'normal'}
+              sortDirection={orderBy === headCell.id ? order : false}
+              style={{ backgroundColor: '#464646' }}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+                style={{ color: 'white' }}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+}
+
 const MainFrame = () => {
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [order, setOrder] = React.useState<Order>('asc');
+    const [orderBy, setOrderBy] = React.useState<keyof Data>('inUse');
+    const [page, setPage] = React.useState(0);
+    const [dense, setDense] = React.useState(false);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    useEffect(() => {
+        // call users api
+    }, []);
+
+    const handleRequestSort = (
+        event: React.MouseEvent<unknown>,
+        property: keyof Data,
+      ) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
 
     const handleChangePage = (event: unknown, newPage: number) => {
         setPage(newPage);
     };
 
     const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(+event.target.value);
+        setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     };
+
+    const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDense(event.target.checked);
+    };
+
+    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+    const visibleRows = React.useMemo(
+        () =>
+        stableSort(rows, getComparator(order, orderBy)).slice(
+            page * rowsPerPage,
+            page * rowsPerPage + rowsPerPage,
+        ),
+        [order, orderBy, page, rowsPerPage],
+    );
 
     const [ data, setData ] = useState('');
 
@@ -176,38 +320,38 @@ const MainFrame = () => {
             <Paper sx={styles.dataPaper}>
                 <TableContainer sx={styles.dataTable}>
                     <Table stickyHeader aria-label="sticky table">
-                        <TableHead>
-                            <TableRow>
-                                {columns.map((column) => (
-                                    <TableCell
-                                        key={column.id}
-                                        align={column.align}
-                                        style={{ minWidth: column.minWidth }}
-                                    >
-                                        {column.label}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        </TableHead>
+                        <EnhancedTableHead
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                        />
                         <TableBody>
-                            {rows
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row) => {
-                                return (
-                                <TableRow hover role="checkbox" tabIndex={-1} key={row.emailAddress}>
-                                    {columns.map((column) => {
-                                    const value = row[column.id];
+                            {
+                                visibleRows.map((row, index) => {
+                                    const labelId = `enhanced-table-checkbox-${index}`;
                                     return (
-                                        <TableCell key={column.id} align={column.align}>
-                                        {column.format && typeof value === 'number'
-                                            ? column.format(value)
-                                            : value}
-                                        </TableCell>
+                                        <TableRow hover role="checkbox" tabIndex={-1} key={row.emailAddress} sx={{ cursor: 'pointer' }}>
+                                            <TableCell
+                                                component="th"
+                                                id={labelId}
+                                                scope="row"
+                                                padding="normal"
+                                            >
+                                                {row.emailAddress}
+                                            </TableCell>
+                                            <TableCell align="right">{row.inUse}</TableCell>
+                                            <TableCell align="right">{row.overdue}</TableCell>
+                                            <TableCell align="right">{row.role}</TableCell>
+                                        </TableRow>
                                     );
-                                    })}
-                                </TableRow>
-                                );
-                            })}
+                                })
+                            }
+                            {
+                                emptyRows > 0 && (
+                                    <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                                        <TableCell colSpan={6} />
+                                    </TableRow>
+                            )}
                         </TableBody>
                     </Table>
                 </TableContainer>
@@ -219,6 +363,10 @@ const MainFrame = () => {
                     page={page}
                     onPageChange={handleChangePage}
                     onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+                <FormControlLabel
+                    control={<Switch checked={dense} onChange={handleChangeDense} />}
+                    label="Dense padding"
                 />
             </Paper>
         </Box>
@@ -377,14 +525,14 @@ const styles = {
     },
 
     dataPaper: { 
-        width: '88%', 
+        width: '95.5%', 
         overflow: 'hidden',
         marginTop: '35px',
         boxShadow: '0'
     },
 
     dataTable: {
-        maxHeight: '630px'
+        maxHeight: `${window.innerHeight - 300}px`
     }
 };
 ///////////////////////////////////////////////////////////////////////
