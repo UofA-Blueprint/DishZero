30c67b87a4b70c14b1de2f732bef7844283abe65
Jared Drueco <drueco03@gmail.com>
2023-09-27 19:34:59 -0600

    fix: borrow not working on type

--- app/src/routes/borrow.tsx
+++ app/src/routes/borrow.tsx
@@ -89,7 +89,6 @@ export default () => {
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
     const previousURL = queryParams.get('previousURL');
-    console.log(previousURL)
     if (previousURL?.includes("dishzero.ca")) {
       const dishID = (previousURL.match(/dishID=([^&]+)/) || "")[1];
       setConfirm(true);
@@ -112,7 +111,8 @@ export default () => {
   const onScan = confirm
     ? null
     : (id: string) => {
-        const dishID = (id.match(/dishID=([^&]+)/) || "")[1];
+        const urlID = id.match(/dishID=([^&]+)/);
+        const dishID = urlID ? urlID[1] : id;
         setScanId(dishID);
         setConfirm(true);
       };
