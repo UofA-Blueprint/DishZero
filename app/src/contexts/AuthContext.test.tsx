8d99459b5c592c51deb6ebd86febc614c98541a9
Kira Muhlbauer <kira@circleci.com>
2023-09-29 12:32:11 -0600

    working on typescript for react
    # Conflicts:
    #	app/package-lock.json

--- app/src/contexts/AuthContext.test.tsx
+++ app/src/contexts/AuthContext.test.tsx
@@ -1,5 +1,14 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { AuthProvider } from "./AuthContext";
+
 describe("AuthContext", () => {
   it("should be false", () => {
     expect(false).toBe(false);
+  });
+
+  it("should render the thing", () => {
+    render(<AuthProvider>{(value) => <span>{value}</span>}</AuthProvider>);
+    expect(screen.getByText("Admin Panel")).toBeInTheDocument();
   });
 });
