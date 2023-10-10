add17759080765ba22a50ab7d401b355cf27d558
Kyle Wiskel <kwiskel@gmail.com>
2023-10-10 14:46:26 -0600

    Added Add New Dish dialog

---
+++ app/src/admin/Dishes/addNewDish.tsx
@@ -0,0 +1,73 @@
+/*eslint-disable*/
+import {
+    Box,
+    Dialog,
+    DialogContent,
+    DialogTitle,
+    Divider,
+    FormControlLabel,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography,
+} from '@mui/material'
+import { useEffect, useState } from 'react'
+import { StyledContainedButton, StyledOutlinedButton } from './dishHeader'
+
+interface Props {
+    open: boolean
+    setOpen: (open: boolean) => void
+}
+
+export default function AddNewDishDialog({ open, setOpen }: Props) {
+    const [dishTypes, setDishTypes] = useState<string[]>([])
+
+    useEffect(() => {
+        setDishTypes(['Mug', 'Dish'])
+    }, [])
+    return (
+        <Dialog
+            open={open}
+            onClose={() => setOpen(false)}
+            sx={{
+                '& .MuiDialog-paper': {
+                    borderRadius: '20px',
+                },
+            }}>
+            <DialogTitle sx={{ textAlign: 'center', fontWeight: 'bold' }}>Add New Dish</DialogTitle>
+            <DialogContent sx={{ minWidth: '420px' }}>
+                <Typography variant="body1" fontWeight="bold" sx={{ mb: '1rem' }}>
+                    Add a single dish
+                </Typography>
+                {'Dish Type:'}
+                <RadioGroup row aria-label="dish-types-radio-group" name="dish-types-radio-group" sx={{ mb: '1rem' }}>
+                    {dishTypes.map((type) => (
+                        <FormControlLabel value={type} control={<Radio />} label={type} />
+                    ))}
+                </RadioGroup>
+                {'Dish ID:'}
+                <Box width="100%" sx={{ textAlign: 'center' }}>
+                    <TextField
+                        variant="outlined"
+                        sx={{ width: '100%', mb: '1rem', mt: '0.25rem' }}
+                        placeholder="Enter dish id..."
+                    />
+                    <StyledContainedButton variant="contained" onClick={() => setOpen(true)} sx={{ width: '90%' }}>
+                        Add new dish
+                    </StyledContainedButton>
+                </Box>
+                <Divider>
+                    <Typography variant="overline" sx={{ mb: '0.25rem' }}>
+                        OR
+                    </Typography>
+                </Divider>
+                <Typography variant="body1" fontWeight="bold" sx={{ mb: '1rem' }}>
+                    Add multiple dishes
+                </Typography>
+                <Box width="100%" sx={{ textAlign: 'center' }}>
+                    <StyledOutlinedButton sx={{ width: '90%' }}>Upload a CSV file</StyledOutlinedButton>
+                </Box>
+            </DialogContent>
+        </Dialog>
+    )
+}
--- app/src/admin/Dishes/dishHeader.tsx
+++ app/src/admin/Dishes/dishHeader.tsx
@@ -1,82 +1,76 @@
-import { Add, IosShare, Search } from "@mui/icons-material";
-import {
-    Box,
-    Button,
-    InputAdornment,
-    TextField,
-    Typography,
-    styled,
-} from "@mui/material";
+import { Add, IosShare, Search } from '@mui/icons-material'
+import { Box, Button, InputAdornment, TextField, Typography, styled } from '@mui/material'
+import { useState } from 'react'
+import AddNewDishDialog from './addNewDish'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const StyledOutlinedButton = styled(Button)(({ theme }) => ({
-    borderRadius: "30px",
-    border: "2px solid #68B49A !important",
-    "&:hover": {
-        backgroundColor: "#68B49A11",
+export const StyledOutlinedButton = styled(Button)(({ theme }) => ({
+    borderRadius: '30px',
+    border: '2px solid #68B49A !important',
+    '&:hover': {
+        backgroundColor: '#68B49A11',
     },
-    padding: "0.5rem 2rem",
-    color: "#68B49A",
-}));
+    padding: '0.5rem 2rem !important',
+    color: '#68B49A',
+}))
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const StyledContainedButton = styled(Button)(({ theme }) => ({
+    borderRadius: '30px',
+    backgroundColor: '#68B49A',
+    '&:hover': {
+        backgroundColor: '#68B49Aee',
+    },
+    padding: '0.5rem 2rem',
+    margin: '1rem',
+}))
 
 export default function AdminDishHeader() {
+    const [open, setOpen] = useState(false)
+
     return (
         <>
-        <Typography variant="h4" fontWeight="bold">
-            Dish Data
-        </Typography>
-        <Box
-            display="flex"
-            flexDirection="row"
-            sx={{ mb: "1rem", justifyContent: "space-between" }}
-            width="100%"
-            alignItems={"center"}
-        >
-            <Box display="flex" alignItems={"center"}>
-            <TextField
-                placeholder="Search dish..."
-                size="small"
-                sx={{
-                color: "#C2C2C2",
-                m: "1rem",
-                "& .MuiOutlinedInput-notchedOutline": {
-                    borderRadius: "30px",
-                    border: "1px solid #464646",
-                },
-                height: "fit-content",
-                }}
-                margin="dense"
-                InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                    <Search />
-                    </InputAdornment>
-                ),
-                }}
-            />
-            <StyledOutlinedButton>Search</StyledOutlinedButton>
+            <Typography variant="h4" fontWeight="bold">
+                Dish Data
+            </Typography>
+            <Box
+                display="flex"
+                flexDirection="row"
+                sx={{ mb: '1rem', justifyContent: 'space-between' }}
+                width="100%"
+                alignItems={'center'}>
+                <Box display="flex" alignItems={'center'}>
+                    <TextField
+                        placeholder="Search dish..."
+                        size="small"
+                        sx={{
+                            color: '#C2C2C2',
+                            m: '1rem',
+                            '& .MuiOutlinedInput-notchedOutline': {
+                                borderRadius: '30px',
+                                border: '1px solid #464646',
+                            },
+                            height: 'fit-content',
+                        }}
+                        margin="dense"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <Search />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <StyledOutlinedButton>Search</StyledOutlinedButton>
+                </Box>
+                <Box display="flex" alignItems={'center'}>
+                    <StyledOutlinedButton startIcon={<IosShare></IosShare>}>Export Data</StyledOutlinedButton>
+                    <StyledContainedButton variant="contained" startIcon={<Add></Add>} onClick={() => setOpen(true)}>
+                        Add new dish
+                    </StyledContainedButton>
+                </Box>
             </Box>
-            <Box display="flex" alignItems={"center"}>
-            <StyledOutlinedButton startIcon={<IosShare></IosShare>}>
-                Export Data
-            </StyledOutlinedButton>
-            <Button
-                variant="contained"
-                startIcon={<Add></Add>}
-                sx={{
-                borderRadius: "30px",
-                backgroundColor: "#68B49A",
-                "&:hover": {
-                    backgroundColor: "#68B49Aee",
-                },
-                padding: "0.5rem 2rem",
-                m: "1rem",
-                }}
-            >
-                Add new dish
-            </Button>
-            </Box>
-        </Box>
+            <AddNewDishDialog open={open} setOpen={setOpen} />
         </>
-    );
+    )
 }
