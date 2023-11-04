1d7765f76a21aaa44bfee3033b5b39c03e9ec4aa
Kira Muhlbauer <kira@circleci.com>
2023-11-04 11:45:34 -0600

    fix build

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "env": {
-      "node": true,
-      "es2021": true
+  env: {
+    node: true,
+    es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
   rules: {
     "no-console": "warn",
     "@typescript-eslint/no-explicit-any": "warn",
