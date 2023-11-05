77ec75b331898f43cbdb08573b68a1b275123a97
Ayush Roy <ayushroy@Ayushs-MacBook-Air.local>
2023-11-04 19:18:17 -0600

    cors fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,9 @@ import { qrCodeRouter } from './routes/qrCode'
 const app = express()
 dotenv.config()
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 app.use(express.json())
 app.use(cookieParser())
 
