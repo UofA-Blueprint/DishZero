fc67350ee3382c63c628cc8d66c924b6ccda7b62
Kyle Wiskel <kylewiskel@Kyles-MacBook-Pro.local>
2024-01-03 20:32:12 -0700

    Added path for dishes admin page

--- app/src/routes/admin.tsx
+++ app/src/routes/admin.tsx
@@ -304,7 +304,8 @@ function sortRows(rows) {
     return tableRows
 }
 
-function Admin() {
+// function Admin() {
+function Admin({ path }: { path: string }) {
     const { currentUser, sessionToken } = useAuth()
     const [dishesUsed, setDishesUsed] = useState<any[]>([])
     const [transactionsUsed, setTransactionsUsed] = useState<any[]>([])
@@ -394,39 +395,42 @@ function Admin() {
 
             {/* on desktop */}
             <BrowserView>
-                <div className="d-flex">
-                    <Toolbar />
-                    <div style={{ marginTop: '48px', marginLeft: '40px', marginRight: '40px', flexGrow: 1 }}>
-                        <p className="sub-header-2">Home</p>
-                        {bar}
-                        <p className="sub-header-2" style={{ marginTop: 40 }}>
-                            Recent transactions
-                        </p>
+                {path == 'dishes' && <AdminDishes />}
+                {path == '' && (
+                    <div className="d-flex">
+                        <Toolbar />
+                        <div style={{ marginTop: '48px', marginLeft: '40px', marginRight: '40px', flexGrow: 1 }}>
+                            <p className="sub-header-2">Home</p>
+                            {bar}
+                            <p className="sub-header-2" style={{ marginTop: 40 }}>
+                                Recent transactions
+                            </p>
 
-                        <div className="d-flex" style={{ marginBottom: '16px' }}>
-                            {/* search Bar */}
+                            <div className="d-flex" style={{ marginBottom: '16px' }}>
+                                {/* search Bar */}
 
-                            <input
-                                className="search-container d-flex"
-                                type="text"
-                                placeholder="Type text here..."
-                                onChange={searchChange}
-                                value={searchInput}
-                                style={{ marginRight: '8px' }}
-                            />
+                                <input
+                                    className="search-container d-flex"
+                                    type="text"
+                                    placeholder="Type text here..."
+                                    onChange={searchChange}
+                                    value={searchInput}
+                                    style={{ marginRight: '8px' }}
+                                />
 
-                            <button className="search-b d-flex" onClick={handleClick}>
-                                <p className="sub-header-3">Search</p>
-                            </button>
+                                <button className="search-b d-flex" onClick={handleClick}>
+                                    <p className="sub-header-3">Search</p>
+                                </button>
 
-                            <div className="d-flex justify-content-end" style={{ width: '100%' }}>
-                                {add}
+                                <div className="d-flex justify-content-end" style={{ width: '100%' }}>
+                                    {add}
+                                </div>
                             </div>
+                            {table}
+                            {row}
                         </div>
-                        {table}
-                        {row}
                     </div>
-                </div>
+                )}
             </BrowserView>
         </SnackbarProvider>
         // </ThemeProvider>
