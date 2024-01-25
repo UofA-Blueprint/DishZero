971e52fba5e098c027ac5940c11ff80285bde43c
Kyle Wiskel <kwiskel@gmail.com>
2024-01-25 09:39:53 -0700

    Remove waste diverted value for now

--- app/src/routes/home.tsx
+++ app/src/routes/home.tsx
@@ -182,7 +182,8 @@ const ExistingUser = (dishesUsed) => {
                         <img src={leaf_white} alt="leaf" style={{ position: 'absolute', top: '16px', right: '16px' }} />
                         <div className="d-flex">
                             <p className="header mb-0" data-testid="waste-diverted-amt">
-                                {returnedDishes * 0.5}
+                                {/* {returnedDishes * 0.5} */}
+                                N/A
                             </p>
                             <p className="sub-header-2 mb-1" style={{ alignSelf: 'end', marginLeft: '7px' }}>
                                 Lbs
